d20275595a226dfb2bb834924ef43445fb592d01
IceTea_1273 <a.makutenaite@gmail.com>
2024-11-27 17:33:49 +0200

    modified:   functions.js 	modified:   index.html 	modified:   style.css

--- functions.js
+++ functions.js
@@ -3,3 +3,22 @@ const toggleButton = document.getElementById('theme_button');
 toggleButton.addEventListener('click', () => {
     document.body.classList.toggle('dark_theme');
 });
+
+const goToTopButton = document.getElementById('go_to_top');
+
+window.addEventListener('scroll', checkHeight);
+
+function checkHeight() {
+    if (window.scrollY > 200) {
+        goToTopButton.style.display = 'block';
+    } else {
+        goToTopButton.style.display = 'none';
+    }
+}
+
+goToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
